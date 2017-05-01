96645e70978fd917cbe281c38af47f2e4cdb7717
8471919 <8471919@naver.com>
2017-05-01 16:37:29 +0900

    Add files via upload

---
+++ ct_201710982/DrawRect2.py
@@ -0,0 +1,14 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+t1.shape("turtle")
+t1.speed(2)
+t1.fd(100)
+t1.rt(90)
+t1.fd(100)
+t1.rt(90)
+t1.fd(100)
+t1.rt(90)
+t1.fd(100)
+
+wn.exitonclick()
